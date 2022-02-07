297289a46b4449b0819dce88ce501c54835d8d9a
miyasan_0301 <miyasan.19990301@gmail.com>
2022-02-07 11:09:59 +0900

    ✅  pokemon-endopoint

--- src/utils/apolloClient.ts
+++ src/utils/apolloClient.ts
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const apolloClient = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  uri: "https://graphql-pokemon2.vercel.app/",
+  // uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
 });
