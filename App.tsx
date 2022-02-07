b7ceeb5fd75044f55c6b489020d3b8cf00efe22c
miyasan_0301 <miyasan.19990301@gmail.com>
2022-02-07 11:20:27 +0900

    🔥 swr

--- App.tsx
+++ App.tsx
@@ -2,10 +2,8 @@ import { ApolloProvider } from "@apollo/client";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { SWRConfig } from "swr";
 
 import { Toaster } from "~/components/Toaster";
-import { getFetcher } from "~/functions/fetcher/unfetch";
 import { useCachedResources } from "~/hooks/useCachedResources";
 import { useColorScheme } from "~/hooks/useColorScheme";
 import { Navigations } from "~/navigations";
@@ -19,19 +17,13 @@ const App = () => {
     return null;
   } else {
     return (
-      <SWRConfig
-        value={{
-          fetcher: getFetcher,
-        }}
-      >
-        <ApolloProvider client={apolloClient}>
-          <SafeAreaProvider>
-            <Navigations colorScheme={colorScheme} />
-            <StatusBar />
-            <Toaster position="bottom-center" />
-          </SafeAreaProvider>
-        </ApolloProvider>
-      </SWRConfig>
+      <ApolloProvider client={apolloClient}>
+        <SafeAreaProvider>
+          <Navigations colorScheme={colorScheme} />
+          <StatusBar />
+          <Toaster position="bottom-center" />
+        </SafeAreaProvider>
+      </ApolloProvider>
     );
   }
 };
