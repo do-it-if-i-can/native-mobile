92e85a99792a8cb40d21525cce7a7a597b43d45d
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-06 17:58:55 +0900

    🚧 TodoScreen

---
+++ src/components/screen/Todo/Todo.screen.tsx
@@ -0,0 +1,18 @@
+import type { FC } from "react";
+import React from "react";
+
+import { LayoutErrorBoundary } from "~/components/functional/Error";
+import { SafeAreaLayout } from "~/components/ui/Layout";
+
+import type { TodoScreenProps } from "./ScreenProps";
+import { Todo } from "./Todo";
+
+export const TodoScreen: FC<TodoScreenProps> = (props) => {
+  return (
+    <LayoutErrorBoundary>
+      <SafeAreaLayout bg="bg2" isCenter>
+        <Todo {...props} />
+      </SafeAreaLayout>
+    </LayoutErrorBoundary>
+  );
+};
