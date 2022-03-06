c9c2fd480184c3d3bd2fccd008d2c993583e051a
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-06 18:05:03 +0900

    ✨ QinTodo

---
+++ src/components/ui/Icon/index.ts
@@ -0,0 +1,5 @@
+export { AntDesignIcon } from "./AntDesignIcon";
+export { Apple } from "./Apple";
+export { FeatherIcon } from "./FeatherIcon";
+export { Google } from "./Google";
+export { QinTodo } from "./QinTodo";
