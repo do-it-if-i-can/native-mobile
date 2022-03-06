696a14578239571c0caf67491c3948810f0ef397
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-07 05:12:52 +0900

    💄 編集時のfocusカラー追加

--- src/styles/theme/bg.tsx
+++ src/styles/theme/bg.tsx
@@ -3,10 +3,12 @@ export const bgTheme = {
     bg0: "#ffffff00",
     bg1: "#FFFFFF",
     bg2: "#F1F5F9",
+    edit: "#FBBF2410",
   },
   dark: {
     bg0: "#00000000",
     bg1: "#373c3f", // 未定
     bg2: "#373c3f", // 未定
+    edit: "#FBBF2410", // 未定
   },
 } as const;
