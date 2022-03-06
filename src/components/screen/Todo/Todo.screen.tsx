87e9f917069bc2ae3b6dc9ba5059d3ecd8b8c8bd
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-07 05:15:05 +0900

    🚧 TodoScreen

--- src/components/screen/Todo/Todo.screen.tsx
+++ src/components/screen/Todo/Todo.screen.tsx
@@ -1,7 +1,10 @@
 import type { FC } from "react";
 import React from "react";
+import { InputAccessoryView } from "react-native";
 
 import { LayoutErrorBoundary } from "~/components/functional/Error";
+import { KeyboardAvoiding } from "~/components/functional/KeyboardAvoiding";
+import { TodoInput } from "~/components/model/todo/TodoInput";
 import { SafeAreaLayout } from "~/components/ui/Layout";
 
 import type { TodoScreenProps } from "./ScreenProps";
@@ -10,8 +13,14 @@ import { Todo } from "./Todo";
 export const TodoScreen: FC<TodoScreenProps> = (props) => {
   return (
     <LayoutErrorBoundary>
-      <SafeAreaLayout bg="bg1" isCenter>
-        <Todo {...props} />
+      <SafeAreaLayout bg="bg1">
+        <KeyboardAvoiding>
+          <Todo {...props} />
+        </KeyboardAvoiding>
+
+        <InputAccessoryView>
+          <TodoInput />
+        </InputAccessoryView>
       </SafeAreaLayout>
     </LayoutErrorBoundary>
   );
--- src/components/screen/Todo/Todo.tsx
+++ src/components/screen/Todo/Todo.tsx
@@ -1,61 +1,64 @@
 import type { FC } from "react";
-import React, { useState } from "react";
-import { InputAccessoryView, ScrollView, StyleSheet } from "react-native";
+import React, { useCallback, useState } from "react";
+import { Button, StyleSheet } from "react-native";
 
-import { TodoInput } from "~/components/model/todo/TodoInput";
-import { Button } from "~/components/ui/Button";
-import { Radio } from "~/components/ui/Radio";
+import { TodoAddButton } from "~/components/model/todo/TodoAddButton";
+import { TodoItem } from "~/components/model/todo/TodoItem";
+import { ActionCheckModal } from "~/components/screen/Account/modal";
 import { Text } from "~/components/ui/Text";
 import { View } from "~/components/ui/View";
 
 import type { TodoScreenProps } from "./ScreenProps";
 
 export const Todo: FC<TodoScreenProps> = () => {
-  const [radio, onChangeValue] = useState("1");
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const onCloseModal = useCallback(() => {
+    setModalVisible(false);
+  }, []);
+
+  const onOpenModal = useCallback(() => {
+    setModalVisible(true);
+  }, []);
 
   return (
     <>
-      <ScrollView keyboardDismissMode="interactive">
-        <View style={style.container}>
-          <Text style={style.message}>サンプルコード</Text>
-          <Text style={style.hint}>⌘ + Shift + Aでテーマ切り替え</Text>
+      <ActionCheckModal isVisible={isModalVisible} onCloseModal={onCloseModal} />
 
-          <Text>default default default</Text>
-          <Text color="color0">color0 color0 color0</Text>
-          <Text color="color1">color1 color1 color1</Text>
-          <Text color="color2">color2 color2 color2</Text>
-          <Text color="primary">primary primary primary</Text>
-          <Text color="secondary">secondary secondary secondary</Text>
-          <Text color="tertiary">tertiary tertiary tertiary</Text>
-          <Text color="accent">accent accent accent</Text>
-          <Text color="danger">danger danger danger</Text>
+      <View style={style.container}>
+        <Text style={style.message}>サンプルコード</Text>
+        <Text style={style.hint}>⌘ + Shift + Aでテーマ切り替え</Text>
+        <Button title="modal open" onPress={onOpenModal} />
 
-          {/* ここはRadioGroup的なProviderで囲って内部でactiveValueを統一させたい */}
-          <Radio activeValue={radio} value="1" onChangeValue={onChangeValue} />
-          <Radio activeValue={radio} value="2" onChangeValue={onChangeValue} bg="secondary" />
-          <Radio activeValue={radio} value="3" onChangeValue={onChangeValue} bg="tertiary" />
+        <Text style={style.status_label} color="primary">
+          今日する
+        </Text>
+        <TodoAddButton />
+        <TodoItem status="TODAY" title="React勉強（編集中）" isEdit />
+        <TodoItem status="TODAY" title="React勉強" />
 
-          <Button label="default" />
-          <Button label="bg0" bg="bg0" isBorder />
-          <Button label="bg1" bg="bg1" isBorder />
-          <Button label="primary" bg="primary" color="white" />
-          <Button label="secondary" bg="secondary" color="white" />
-          <Button label="tertiary" bg="tertiary" color="white" />
-          <Button label="accent" bg="accent" color="white" />
-          <Button label="danger" bg="danger" color="white" />
-        </View>
-      </ScrollView>
+        <Text style={style.status_label} color="secondary">
+          明日する
+        </Text>
+        <TodoAddButton />
+        <TodoItem status="TOMORROW" title="React勉強" />
+        <TodoItem status="TOMORROW" title="React勉強" />
+        <TodoItem status="TOMORROW" title="React勉強" />
 
-      <InputAccessoryView>
-        <TodoInput />
-      </InputAccessoryView>
+        <Text style={style.status_label} color="tertiary">
+          今度する
+        </Text>
+        <TodoAddButton />
+        <TodoItem status="SOMEDAY" title="React勉強" />
+        <TodoItem status="SOMEDAY" title="React勉強" />
+      </View>
     </>
   );
 };
 
 const style = StyleSheet.create({
   container: {
-    padding: "6%",
+    padding: "3%",
   },
   //
   message: {
@@ -67,5 +70,12 @@ const style = StyleSheet.create({
   hint: {
     textAlign: "center",
     marginBottom: "5%",
+  },
+
+  status_label: {
+    fontSize: 24,
+    fontWeight: "600",
+    marginVertical: "4%",
+    paddingLeft: "2%",
   },
 });
