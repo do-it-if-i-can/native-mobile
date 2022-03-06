ecb17627ceb3c886815321738b73eeb56cbe8788
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-07 03:16:56 +0900

    ♻️ 共通スタイルをリセット

--- src/styles/textInput.styles.ts
+++ src/styles/textInput.styles.ts
@@ -1,13 +1,3 @@
 import { StyleSheet } from "react-native";
 
-export const textInputStyles = StyleSheet.create({
-  bg: {
-    padding: 12,
-    borderRadius: 10,
-  },
-  half: {
-    width: "100%",
-    padding: 12,
-    borderRadius: 10,
-  },
-});
+export const textInputStyles = StyleSheet.create({});
--- src/styles/view.styles.ts
+++ src/styles/view.styles.ts
@@ -1,15 +1,3 @@
 import { StyleSheet } from "react-native";
 
-export const viewStyles = StyleSheet.create({
-  horizontal: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  flex1: {
-    flex: 1,
-  },
-  space: {
-    width: "5%",
-  },
-});
+export const viewStyles = StyleSheet.create({});
