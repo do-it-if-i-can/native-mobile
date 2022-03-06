02ed98d6612b3cb9e6c273751d889d3726d34e4f
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-06 18:01:30 +0900

    ✨ PrevButton

---
+++ src/components/ui/Button/PrevButton.tsx
@@ -0,0 +1,32 @@
+import { MaterialIcons } from "@expo/vector-icons";
+import type { VFC } from "react";
+import React, { useCallback } from "react";
+import { StyleSheet } from "react-native";
+
+import { BounceableView } from "~/components/ui/View";
+import { useThemeColor } from "~/hooks/useThemeColor";
+import type { RootStackParamList, StackScreenProps } from "~/types";
+
+type PrevProps = StackScreenProps<keyof RootStackParamList> & {
+  screen: "TodoScreen";
+};
+
+export const PrevButton: VFC<PrevProps> = ({ screen, navigation }) => {
+  const icon = useThemeColor({}, "icon2");
+
+  const onPrevScreen = useCallback(() => {
+    navigation.navigate(screen);
+  }, [navigation, screen]);
+
+  return (
+    <BounceableView viewStyle={[defaultStyle.button]} activeScale={0.9} onPress={onPrevScreen}>
+      <MaterialIcons name="keyboard-arrow-left" size={40} color={icon} />
+    </BounceableView>
+  );
+};
+
+const defaultStyle = StyleSheet.create({
+  button: {
+    width: "auto",
+  },
+});
