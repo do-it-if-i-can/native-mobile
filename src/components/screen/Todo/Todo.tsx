e2ce692b45a7291110a2cc2d5b42dfe68ae42764
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-07 13:04:21 +0900

    🚧 サンプル画面

--- src/components/screen/Todo/Todo.tsx
+++ src/components/screen/Todo/Todo.tsx
@@ -1,58 +1,47 @@
 import type { FC } from "react";
-import React, { useCallback, useState } from "react";
+import React from "react";
 import { Button, StyleSheet } from "react-native";
 
 import { TodoAddButton } from "~/components/model/todo/TodoAddButton";
 import { TodoItem } from "~/components/model/todo/TodoItem";
-import { ActionCheckModal } from "~/components/screen/Account/modal";
 import { Text } from "~/components/ui/Text";
 import { View } from "~/components/ui/View";
 
 import type { TodoScreenProps } from "./ScreenProps";
 
-export const Todo: FC<TodoScreenProps> = () => {
-  const [isModalVisible, setModalVisible] = useState(false);
-
-  const onCloseModal = useCallback(() => {
-    setModalVisible(false);
-  }, []);
-
-  const onOpenModal = useCallback(() => {
-    setModalVisible(true);
-  }, []);
+export const Todo: FC<TodoScreenProps> = ({ navigation }) => {
+  const onPushSetting = () => {
+    navigation.navigate("SettingNavigator");
+  };
 
   return (
-    <>
-      <ActionCheckModal isVisible={isModalVisible} onCloseModal={onCloseModal} />
+    <View style={style.container}>
+      <Text style={style.message}>サンプルコード</Text>
+      <Text style={style.hint}>⌘ + Shift + Aでテーマ切り替え</Text>
+      <Button title="push setting modal" onPress={onPushSetting} />
 
-      <View style={style.container}>
-        <Text style={style.message}>サンプルコード</Text>
-        <Text style={style.hint}>⌘ + Shift + Aでテーマ切り替え</Text>
-        <Button title="modal open" onPress={onOpenModal} />
+      <Text style={style.status_label} color="primary">
+        今日する
+      </Text>
+      <TodoAddButton />
+      <TodoItem status="TODAY" title="React勉強（編集中）" isEdit />
+      <TodoItem status="TODAY" title="React勉強" />
 
-        <Text style={style.status_label} color="primary">
-          今日する
-        </Text>
-        <TodoAddButton />
-        <TodoItem status="TODAY" title="React勉強（編集中）" isEdit />
-        <TodoItem status="TODAY" title="React勉強" />
+      <Text style={style.status_label} color="secondary">
+        明日する
+      </Text>
+      <TodoAddButton />
+      <TodoItem status="TOMORROW" title="React勉強" />
+      <TodoItem status="TOMORROW" title="React勉強" />
+      <TodoItem status="TOMORROW" title="React勉強" />
 
-        <Text style={style.status_label} color="secondary">
-          明日する
-        </Text>
-        <TodoAddButton />
-        <TodoItem status="TOMORROW" title="React勉強" />
-        <TodoItem status="TOMORROW" title="React勉強" />
-        <TodoItem status="TOMORROW" title="React勉強" />
-
-        <Text style={style.status_label} color="tertiary">
-          今度する
-        </Text>
-        <TodoAddButton />
-        <TodoItem status="SOMEDAY" title="React勉強" />
-        <TodoItem status="SOMEDAY" title="React勉強" />
-      </View>
-    </>
+      <Text style={style.status_label} color="tertiary">
+        今度する
+      </Text>
+      <TodoAddButton />
+      <TodoItem status="SOMEDAY" title="React勉強" />
+      <TodoItem status="SOMEDAY" title="React勉強" />
+    </View>
   );
 };
 
