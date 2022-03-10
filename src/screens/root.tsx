04065d71ec2c704a5d58fd69da0a1e45c746771f
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-10 12:24:03 +0900

    ✨ メイン画面にヘッダー作成

--- src/screens/root.tsx
+++ src/screens/root.tsx
@@ -2,6 +2,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import type { FC } from "react";
 import React from "react";
 
+import { Avatar } from "~/components/model/user/Avatar";
+import { QinTodo } from "~/components/ui/Icon";
 import { ActivityIndicator } from "~/components/ui/Progress";
 import { useListenSession } from "~/hooks/useListenSession";
 import { useThemeColor } from "~/hooks/useThemeColor";
@@ -23,15 +25,37 @@ export const RootNavigator: FC = () => {
     <RootStack.Navigator
       initialRouteName={session.route}
       screenOptions={{
-        headerShown: false,
         headerStyle: { backgroundColor },
       }}
     >
-      <RootStack.Screen name="NotFoundScreen" component={NotFoundScreen} options={{ title: "Oops!" }} />
+      <RootStack.Screen
+        name="NotFoundScreen"
+        component={NotFoundScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
 
-      <RootStack.Screen name="SignInScreen" component={SignInScreen} options={{}} />
+      <RootStack.Screen
+        name="SignInScreen"
+        component={SignInScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
 
-      <RootStack.Screen name="TodoScreen" component={TodoScreen} options={{}} />
+      <RootStack.Screen
+        name="TodoScreen"
+        component={TodoScreen}
+        options={({ navigation }) => ({
+          headerShadowVisible: false,
+          headerTitle: () => <QinTodo width={120} height={40} />,
+          headerRight: () => {
+            const onNavigation = () => navigation.navigate("SettingNavigator");
+            return <Avatar onPress={onNavigation} />;
+          },
+        })}
+      />
 
       <RootStack.Group screenOptions={{ presentation: "fullScreenModal" }}>
         <RootStack.Screen name="SettingNavigator" component={SettingNavigator} />
