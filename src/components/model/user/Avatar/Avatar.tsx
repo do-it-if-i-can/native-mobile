5fa79a466017ac579bffbc194562c93d4329554e
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-12 20:14:28 +0900

    ✨ 状態管理のuserからavatarを参照する

--- src/components/model/user/Avatar/Avatar.tsx
+++ src/components/model/user/Avatar/Avatar.tsx
@@ -1,20 +1,34 @@
 import type { FC } from "react";
 import React from "react";
-import type { ImageSourcePropType } from "react-native";
-import { Image } from "react-native";
+import { Image, StyleSheet } from "react-native";
+import { useRecoilValue } from "recoil";
 
 import { DummyAvatar } from "~/components/ui/DummyAvatar";
 import { TouchableOpacity } from "~/components/ui/View";
+import { user } from "~/stores/user";
 
 type Props = {
-  source?: ImageSourcePropType;
   onPress?: () => void;
 };
 
-export const Avatar: FC<Props> = ({ source, onPress }) => {
+export const Avatar: FC<Props> = ({ onPress }) => {
+  const authSession = useRecoilValue(user);
+
   return (
     <TouchableOpacity activeOpacity={0.6} onPress={onPress}>
-      {source ? <Image source={source} /> : <DummyAvatar size={40} />}
+      {authSession?.user ? (
+        <Image source={{ uri: authSession.user.avatar }} style={style.avatar} />
+      ) : (
+        <DummyAvatar size={40} />
+      )}
     </TouchableOpacity>
   );
 };
+
+const style = StyleSheet.create({
+  avatar: {
+    width: 40,
+    height: 40,
+    borderRadius: 999,
+  },
+});
