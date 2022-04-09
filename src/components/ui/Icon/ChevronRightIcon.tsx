ffcc665e0a21ca836b1b66822d5370060c90d55c
miyasan_0301 <miyasan.19990301@gmail.com>
2022-04-09 13:14:43 +0900

    ✨ HeroIcon追加

---
+++ src/components/ui/Icon/ChevronRightIcon.tsx
@@ -0,0 +1,25 @@
+import type { FC } from "react";
+import React, { memo } from "react";
+import Svg, { Path } from "react-native-svg";
+
+import { useThemeColor } from "~/hooks/useThemeColor";
+import type { HeroIconStyle } from "~/types/style";
+
+type Props = HeroIconStyle;
+
+export const ChevronRightIcon: FC<Props> = memo(({ icon = "icon1", lightIcon, darkIcon, size = 24 }) => {
+  const iconColor = useThemeColor({ light: lightIcon, dark: darkIcon }, icon);
+
+  return (
+    <Svg
+      width={size}
+      height={size}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      stroke={iconColor}
+      strokeWidth={1.5}
+    >
+      <Path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
+    </Svg>
+  );
+});
--- src/components/ui/Icon/index.ts
+++ src/components/ui/Icon/index.ts
@@ -1,5 +1,8 @@
 export { Apple } from "./Apple";
+export { ChevronLeftIcon } from "./ChevronLeftIcon";
+export { ChevronRightIcon } from "./ChevronRightIcon";
 export { Google } from "./Google";
 export { PlusCircleIcon } from "./PlusCircleIcon";
 export { PlusIcon } from "./PlusIcon";
 export { QinTodo } from "./QinTodo";
+export { XIcon } from "./XIcon";
