ebeba76b22f6b23aa5c13214ccf38b6720d2306d
miyasan_0301 <miyasan.19990301@gmail.com>
2022-04-10 22:26:47 +0900

    ✨ rc-swipeout

--- src/components/model/todo/TodoItem/TodoItem.tsx
+++ src/components/model/todo/TodoItem/TodoItem.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-handler-names */
+import Swipeout from "rc-swipeout";
 import type { FC } from "react";
 import React, { memo, useMemo, useState } from "react";
 import { StyleSheet } from "react-native";
@@ -27,16 +29,37 @@ export const TodoItem: FC<Props> = memo((props) => {
   }, [props.status]);
 
   return (
-    <View style={style.container} bg={props.isEdit ? "edit" : "bg0"}>
-      <Radio bg={RadioTheme} activeValue={radio} value={true} onChangeValue={onChangeValue} />
-      <Text
-        // eslint-disable-next-line react-native/no-inline-styles
-        style={[style.todo_text, radio ? { textDecorationLine: "line-through" } : null]}
-        color={radio ? "color2" : "color1"}
-      >
-        {props.title}
-      </Text>
-    </View>
+    <Swipeout
+      left={[
+        {
+          text: "reply",
+          onPress: () => console.log("reply"),
+          style: { backgroundColor: "orange", color: "white" },
+          className: "custom-class-1",
+        },
+      ]}
+      right={[
+        {
+          text: "delete",
+          onPress: () => console.log("delete"),
+          style: { backgroundColor: "red", color: "white" },
+          className: "custom-class-2",
+        },
+      ]}
+      onOpen={() => console.log("open")}
+      onClose={() => console.log("close")}
+    >
+      <View style={style.container} bg={props.isEdit ? "edit" : "bg0"}>
+        <Radio bg={RadioTheme} activeValue={radio} value={true} onChangeValue={onChangeValue} />
+        <Text
+          // eslint-disable-next-line react-native/no-inline-styles
+          style={[style.todo_text, radio ? { textDecorationLine: "line-through" } : null]}
+          color={radio ? "color2" : "color1"}
+        >
+          {props.title}
+        </Text>
+      </View>
+    </Swipeout>
   );
 });
 
